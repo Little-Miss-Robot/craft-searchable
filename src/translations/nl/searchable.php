456b972a2ed3ba2285800220a0af604b3b24eb50
Ferre Lambert <ferre@littlemissrobot.com>
2024-02-22 21:06:41 +0100

    feat: add nl translations

---
+++ src/translations/nl/searchable.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    '{name} plugin loaded' => '{name} plugin ingeladen',
+    'This field\'s values are used as search keywords.' => 'De waarden van dit veld worden gebruikt als zoekwoorden.'
+];
